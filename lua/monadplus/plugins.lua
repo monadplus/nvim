13e45b9d5364c86b7cba90aaf817d86f1ad89e0a
Arnau Abella <arnauabella@gmail.com>
2023-04-01 10:14:55 +0200

    Update telescope

--- lua/monadplus/plugins.lua
+++ lua/monadplus/plugins.lua
@@ -87,7 +87,7 @@ return require('packer').startup(function(use)
   }
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
+    tag = '0.1.1',
     requires = {
       'nvim-lua/plenary.nvim',
     }

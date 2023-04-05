0117bf1094a71d517c5a934192f2ac3b35ff718f
Arnau Abella <arnauabella@gmail.com>
2023-04-05 08:53:25 +0200

    Enable indent on treesitter
    Does nothing apparently

--- lua/monadplus/plugins/treesitter.lua
+++ lua/monadplus/plugins/treesitter.lua
@@ -4,6 +4,7 @@ if not ok then
 end
 
 require 'nvim-treesitter.configs'.setup {
+
   ensure_installed = {
     "agda",
     "bash",
@@ -35,7 +36,6 @@ require 'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
@@ -46,6 +46,22 @@ require 'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = {"org"},
+    additional_vim_regex_highlighting = { "org" },
+  },
+
+  indent = {
+    enabled = true
+  },
+
+  -- Not working as expected
+  incremental_selection = {
+    enable = false,
+    keymaps = {
+      -- set to `false` to disable one of the mappings
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
   },
 }

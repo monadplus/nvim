31434fec8b3f416c84e5882d7930bd3ecccad05a
Arnau Abella <arnauabella@gmail.com>
2023-05-09 21:10:10 +0200

    Remove php support

--- lua/monadplus/plugins/lsp.lua
+++ lua/monadplus/plugins/lsp.lua
@@ -286,8 +286,6 @@ lsp_custom_settings.yamlls = {} -- schema validation must be configured here
 lsp_custom_settings.ccls = {} -- schema validation must be configured here
 --More at https://github.com/MaskRay/ccls/wiki/Project-Setup
 
--- php
-lsp_custom_settings.intelephense = {}
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 

5923a008fc18a5e56a9f3383644f48adc6fb5aab
Arnau Abella <arnauabella@gmail.com>
2023-11-11 22:29:06 +0100

    Disable copilot

--- lua/monadplus/plugins.lua
+++ lua/monadplus/plugins.lua
@@ -70,7 +70,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
-  use { "zbirenbaum/copilot.lua" }
+  -- use { "zbirenbaum/copilot.lua" } -- Disabled: price > usefulness
 
   -- Snippets
   use 'L3MON4D3/LuaSnip'

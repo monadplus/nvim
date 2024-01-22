15b74ddfae6871a6c2f931e5ea802c49681ac63c
Arnau Abella <arnauabella@gmail.com>
2024-01-22 19:10:54 +0100

    Pin nvim-treesitter while https://github.com/Mofiqul/dracula.nvim/issues/122 is resolved

--- lua/monadplus/plugins.lua
+++ lua/monadplus/plugins.lua
@@ -37,6 +37,7 @@ return require('packer').startup(function(use)
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    tag = 'v0.9.2'
   }
   use 'nvim-treesitter/nvim-treesitter-context'
 

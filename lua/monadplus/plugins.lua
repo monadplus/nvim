ce1fd6e0314cb56c182472035afa4da9f4803f98
Arnau Abella <arnauabella@gmail.com>
2024-01-24 19:21:20 +0100

    Unpin nvim-treesitter
    https://github.com/Mofiqul/dracula.nvim/pull/123 fixed the highlight issue

--- lua/monadplus/plugins.lua
+++ lua/monadplus/plugins.lua
@@ -37,7 +37,6 @@ return require('packer').startup(function(use)
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    tag = 'v0.9.2'
   }
   use 'nvim-treesitter/nvim-treesitter-context'
 

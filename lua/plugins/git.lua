515b0845b4d5fc77f59d781d78f431a705c5afd4
Arnau Abella <arnauabella@gmail.com>
2024-08-10 22:45:09 +0200

    Update deps + fix breaking changes

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -288,29 +288,40 @@ return {
     config = function()
       require("gitsigns").setup {
         signs                        = {
-          add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-          change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-          delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-          topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-          changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
         },
+        signs_staged                 = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged_enable          = true,
         signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
         numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir                 = {
-          interval = 1000,
           follow_files = true
         },
-        attach_to_untracked          = true,
+        auto_attach                  = true,
+        attach_to_untracked          = false,
         current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts      = {
           virt_text = true,
           virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 0,
+          delay = 1000,
           ignore_whitespace = false,
+          virt_text_priority = 100,
         },
-        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
         sign_priority                = 6,
         update_debounce              = 100,
         status_formatter             = nil,   -- Use default
@@ -322,9 +333,6 @@ return {
           relative = 'cursor',
           row = 0,
           col = 1
-        },
-        yadm                         = {
-          enable = false
         },
         on_attach                    = function(bufnr)
           local gs = package.loaded.gitsigns
@@ -367,7 +375,6 @@ return {
           map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { silent = true, noremap = true, desc = "Hunk" })
         end
       }
-      vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { link = 'Visual' })
     end,
   },
 }
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -7,9 +7,8 @@ return {
     event = "VeryLazy",
     config = function()
       local wk = require("which-key")
-
-      -- :h which-key.txt
       wk.setup {
+        preset = "helix",
         plugins = {
           marks = true,
           registers = true,
@@ -27,68 +26,35 @@ return {
             g = true,
           },
         },
-        operators = {},
-        key_labels = {
-          -- override the label used to display some keys.
-          ["<leader>"] = "SPC",
-          ["<localleader>"] = "SPC",
-          ["<space>"] = "SPC",
-          ["<cr>"] = "RET",
-          ["<tab>"] = "TAB",
+        replace = {
+          key = {
+            { "<Leader>",      "SPC" },
+            { "<Localleader>", "SPC" },
+            { "<Space>",       "SPC" },
+            { "<Cr>",          "RET" },
+            { "<Tab>",         "TAB" }
+          }
         },
-        icons = {
-          breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-          separator = "➜", -- symbol used between a key and it's label
-          group = "+", -- symbol prepended to a group
-        },
-        popup_mappings = {
-          scroll_down = '<c-d>', -- binding to scroll down inside the popup
-          scroll_up = '<c-u>',   -- binding to scroll up inside the popup
-        },
-        window = {
-          border = "double",        -- none, single, double, shadow
-          position = "bottom",      -- bottom, top
-          margin = { 0, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
-          padding = { 1, 3, 1, 3 }, -- extra window padding [top, right, bottom, left]
-          winblend = 0              -- [0, 100]
-        },
-        layout = {
-          height = { min = 4, max = 25 },                                             -- min and max height of the columns
-          width = { min = 20, max = 50 },                                             -- min and max width of the columns
-          spacing = 10,                                                               -- spacing between columns
-          align = "left",                                                             -- align columns left, center or right
-        },
-        ignore_missing = false,                                                       -- enable this to hide mappings for which you didn't specify a label
-        hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-        show_help = false,                                                            -- show help message on the command line when the popup is visible
-        triggers = "auto",                                                            -- automatically setup triggers
-        triggers_blacklist = {
-          i = { "j", "k" },
-          v = { "j", "k" },
-        },
+        show_help = false,
         disable = {
-          buftypes = {},
-          filetypes = { "TelescopePrompt" },
+          bt = {},
+          ft = { "TelescopePrompt" },
         },
       }
 
-      wk.register({
-        ["<leader>"] = {
-          -- You can rename mappings here
-          -- e.g. f = { name = "+file", ['n'] = "foo"}
-          b = { name = "+buffer" },
-          c = { name = "+code" },
-          d = { name = "+diffview" },
-          f = { name = "+file" },
-          g = { name = "+git" },
-          h = { name = "+gitsigns" },
-          m = { name = "+lsp" },
-          o = { name = "+open" },
-          r = { name = "+repl" },
-          s = { name = "+search" },
-          t = { name = "+toggle" },
-          x = { name = "+trouble" },
-        }
+      wk.add({
+        { "<leader>b", desc = "+buffer" },
+        { "<leader>c", desc = "+code" },
+        { "<leader>d", desc = "+diffview" },
+        { "<leader>f", desc = "+file" },
+        { "<leader>g", desc = "+git" },
+        { "<leader>h", desc = "+gitsigns" },
+        { "<leader>m", desc = "+lsp" },
+        { "<leader>o", desc = "+open" },
+        { "<leader>r", desc = "+repl" },
+        { "<leader>s", desc = "+search" },
+        { "<leader>t", desc = "+toggle" },
+        { "<leader>x", desc = "+trouble" },
       }, {})
     end
   },

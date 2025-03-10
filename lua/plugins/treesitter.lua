9a12f7da9fea94cbcc1e86aa9b6f683219ba1a6d
Arnau Abella <arnauabella@gmail.com>
2025-03-10 09:52:42 +0100

    Update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -86,7 +86,6 @@ return {
           "markdown",
           "markdown_inline",
           "nix",
-          "org",
           "rust",
           "sql",
           "yaml",
@@ -122,7 +121,7 @@ return {
           -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
           -- Using this option may slow down your editor, and you may see some duplicate highlights.
           -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = { "org" },
+          additional_vim_regex_highlighting = {},
         },
 
         indent = {

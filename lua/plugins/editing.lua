8a669c4853dc0b0b4e35fbd1d598bbaf8db88b6a
Arnau Abella <arnauabella@gmail.com>
2025-11-09 12:25:18 +0100

    Spectre open split

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -103,6 +103,23 @@ return {
         noremap = false,
         desc = "Search on current file",
       },
+    },
+    mapping = {
+      ['open_in_vsplit'] = {
+        map = "<c-v>",
+        cmd = "<cmd>lua vim.cmd('vsplit ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+        desc = "open in vertical split"
+      },
+      ['open_in_split'] = {
+        map = "<c-s>",
+        cmd = "<cmd>lua vim.cmd('split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+        desc = "open in horizontal split"
+      },
+      ['open_in_tab'] = {
+        map = "<c-t>",
+        cmd = "<cmd>lua vim.cmd('tab split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+        desc = "open in new tab"
+      },
     }
   },
 
